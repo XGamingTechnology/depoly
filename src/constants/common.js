2fa1bb019f35b0cdc810a8c1321c734bd89e8c1f
Xblader <gloryofmaster90@gmail.com>
2024-07-30 11:40:29 +0700

    menambahkan bagian constatns common.js menambahkan email baru

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,13 +1,13 @@
 const contact = {
   sg: {
     phone1: '+65 65717440',
-    email: 'enquiries@tridentwater.com.sg',
+    email: 'esi@ptesi.com',
     address: '60 Paya Lebar Road #10-08 Paya Lebar Square. Singapore 409051',
   },
   id: {
     phone1: '+62 21 390 5533',
     phone2: '+62 21 314 5333',
-    email: 'esi@tridentwater.com.sg',
+    email: 'esi@ptesi.com',
     address:
       'Wisma Aria 2nd Floor Suite 202Jl. HOS. Cokroaminoto No. 81Jakarta Pusat 10310',
   },
